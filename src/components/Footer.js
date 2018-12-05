de14176632b5fc73f34ac25eeaaace83814d8073
Sachin Saini <sachinsaini@197NODMB21883.local>
2018-12-05 12:34:37 +0530

    Some styling updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,7 @@ function Footer() {
         <footer className="row col-10 offset-1" id="footer">
                 <ul>
                     <li>Make sure to attempt answer correctly with in given time-frame.</li>
-                    <li>On an average, each question attempt time is 5 Seconds.</li>
-                    <li>For winnng, your accuracy should be greater than 75%.</li>
+                    <li>For winnng, Accuracy >= 75% and Total Questions attempted >= Round_Of(Total Time / 5 Seconds)</li>
                     <li>After the time-lapse, it shows your result accordingly.</li>
                 </ul>
         </footer>
