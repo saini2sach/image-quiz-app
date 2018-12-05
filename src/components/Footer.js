712895423d119d41e3ff2397fc3f25281c54435c
Sachin Saini <sachinsaini@197NODMB21883.local>
2018-12-05 12:26:53 +0530

    Updated the code for small devices. Made responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ function Footer() {
         <footer className="row col-10 offset-1" id="footer">
                 <ul>
                     <li>Make sure to attempt answer correctly with in given time-frame.</li>
+                    <li>On an average, each question attempt time is 5 Seconds.</li>
+                    <li>For winnng, your accuracy should be greater than 75%.</li>
                     <li>After the time-lapse, it shows your result accordingly.</li>
                 </ul>
         </footer>
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,20 +8,20 @@ import '../css/loader.css';
 function MainContent(props) {
     return (
         <div className="col-10 offset-1 main-content clearfix">
-            <div className="col-sm-11 col-md-4 float-left item-image">
+            <div className="col-lg-4 float-left item-image">
                 { props.imageUrl 
                     ? <img src={props.imageUrl} className="author-image" alt={props.correctAnswer} height="250px" />
                     : <div className="loader"></div>
                 }
             </div>
-            <div className="float-left col-md-2 timer-comp">
+            <div className="float-left col-lg-2 timer-comp">
                 <Timer totalSecs={props.timerLimit} showResult={props.showResult}/>
             </div>
             { props.imageUrl 
-                ? <div className="col-md-5 float-right item-options">
+                ? <div className="col-lg-5 float-right item-options">
                     {props.setOfAnswer.map( (item, index) => <Option item={item} key={index} enableContinueBtn={props.enableContinueBtn} answer={props.correctAnswer}/>)}
                   </div>
-                : <div className="col-md-5 float-right item-options option-loader">
+                : <div className="col-lg-5 float-right item-options option-loader">
                     <div className="loader"></div>
                   </div>
             }
