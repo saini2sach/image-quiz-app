f895da404f00ece42142f49324298327ed6f4a4c
Sachin Saini <sachin.saini@paytminsurance.co.in>
2020-04-24 23:35:06 +0530

    Updates

---
+++ precache-manifest.ce4f2fe38d906e34d8cb39eaddeae3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cb3e7efcbdb544d45810fae1cf9104",
+    "url": "/image-quiz-app/index.html"
+  },
+  {
+    "revision": "f6fae85085a708edfd6b",
+    "url": "/image-quiz-app/static/css/main.57aded4c.chunk.css"
+  },
+  {
+    "revision": "907a5cc32415eefa6b55",
+    "url": "/image-quiz-app/static/js/2.0f1dc95f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-quiz-app/static/js/2.0f1dc95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6fae85085a708edfd6b",
+    "url": "/image-quiz-app/static/js/main.7b231868.chunk.js"
+  },
+  {
+    "revision": "a90195b9bd19ff2c7040",
+    "url": "/image-quiz-app/static/js/runtime-main.8e6893f6.js"
+  }
+]);
